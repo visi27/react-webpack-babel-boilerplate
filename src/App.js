33b7fbfeb35d2dbb79d98bb80360cddb1ddf16a0
Evis Bregu <evis.bregu@gmail.com>
2018-03-23 11:24:13 +0100

    Setting up test environment, added simple test application counter

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+export const doIncrement = (prevState) => ({
+  counter: prevState.counter + 1
+});
+
+export const doDecrement = (prevState) => ({
+  counter: prevState.counter - 1
+});
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <div>
+        <h1>My Counter</h1>
+        <Counter counter={counter}/>
+
+        <button
+          type="button"
+          onClick={this.onIncrement}
+        >
+          Increment
+        </button>
+
+        <button
+          type="button"
+          onClick={this.onDecrement}
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+export const Counter = ({counter}) =>
+  <p>{counter}</p>;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const title = 'React Webpack Babel Boilerplate';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App/>,
   document.getElementById('app')
 );
 
