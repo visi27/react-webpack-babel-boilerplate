ea6ebb836c1f99b433fd48ff593c475f44d7bc43
Evis Bregu <evis.bregu@gmail.com>
2018-03-26 17:09:54 +0200

    Experimenting with cypress

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -1,5 +1,8 @@
 describe('App E2E', () => {
-  it('should assert that true is equal to true', () => {
-    expect(true).to.equal(true);
+  it('should have a header', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.get('h1')
+      .should('have.text', 'My Counter');
   });
 });
