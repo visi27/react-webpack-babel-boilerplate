0e97b4bea9a093da3c217d02f84d30d6b6c660bc
Evis Bregu <evis.bregu@gmail.com>
2018-03-26 17:21:55 +0200

    Adding interaction to cypress test

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -1,8 +1,21 @@
 describe('App E2E', () => {
   it('should have a header', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
 
     cy.get('h1')
       .should('have.text', 'My Counter');
+  });
+
+  it('should increment and decrement a counter', () => {
+    cy.get('p').should('have.text', '12');
+
+    cy.contains('Increment').click();
+    cy.get('p').should('have.text', '13');
+
+    cy.contains('Decrement').click();
+    cy.get('p').should('have.text', '12');
+
+    cy.contains('Decrement').click();
+    cy.get('p').should('have.text', '11');
   });
 });
